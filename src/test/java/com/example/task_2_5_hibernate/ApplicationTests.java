a36afa2ea1a1990c0de29bce4a8f0b41f4a09eeb
Dmytro Tkachuk <geekhubme@gmail.com>
2023-03-31 17:06:11 +0300

    INITIAL

---
+++ src/test/java/com/example/task_2_5_hibernate/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_2_5_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
